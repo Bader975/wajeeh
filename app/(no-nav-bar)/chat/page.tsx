9e342ecd3b7725d58adee2e1d766b2fc8d39531a
Bader  Alyami <80196102+Bader975@users.noreply.github.com>
2024-07-20 16:02:13 +0300

    Footer no more needed also

--- app/(no-nav-bar)/chat/page.tsx
+++ app/(no-nav-bar)/chat/page.tsx
@@ -11,7 +11,6 @@ export default function ChatPage() {
           <MainChat />
         </div>
       </div>
-      <Footer />
     </>
   );
 }
--- app/(no-nav-bar)/dashboard/page.tsx
+++ app/(no-nav-bar)/dashboard/page.tsx
@@ -6,7 +6,6 @@ export default function DashboardPage() {
     <>
       <SecondNav />
       <Dashboard />
-      <Footer />
     </>
   );
 }
